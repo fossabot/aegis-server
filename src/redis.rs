dd2277c3c2b9825b78751e12a4cf3eaf7915cb41
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-25 15:11:26 +1100

    redis get and set functions

--- src/redis.rs
+++ src/redis.rs
@@ -5,3 +5,13 @@ pub fn create_client(redis_url: &str) -> redis::Client {
   let client = redis::Client::open(redis_url).unwrap();
   return client;
 }
+
+pub fn push_key_to_redis(client: redis::Client, key: &str, val: &str) {
+  let mut con = client.get_connection();
+  let _ : () = con.set(key, val);
+}
+
+pub fn pull_key_from_redis(client: redis::Client, key: &str) -> &redis::RedisResult<isize> {
+  let mut con = client.get_connection();
+  return con.get(key);
+}
