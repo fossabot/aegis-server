ef58cf9697adc0ddb6e673a00576bfee7eb1728a
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-25 15:33:55 +1100

    make create_client public

--- src/redis.rs
+++ src/redis.rs
@@ -2,7 +2,7 @@ extern crate redis;
 use redis::{Client, Commands, Connection, RedisResult};
 use std::collections::HashSet;
 
-fn create_client(url: &str) -> Connection {
+pub fn create_client(url: &str) -> Connection {
     let client = Client::open(url).unwrap();
     let conn = client.get_connection().unwrap();
     return conn;
