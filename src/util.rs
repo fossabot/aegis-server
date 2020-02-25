b3f93c2eb933cf2dd7ee4097aa7703524a3a8be7
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-25 14:50:19 +1100

    Some function stubs

--- src/util.rs
+++ src/util.rs
@@ -5,3 +5,15 @@ pub fn generate_timestamp() -> DateTime<Local> {
   let local: DateTime<Local> = Local::now();
   return local;
 }
+
+pub fn pretty_print(input: &str) {
+  println!("{:#?}", input);
+}
+
+pub fn push_key_to_redis(key: &str, val: &str) {
+  // Noting here yet....
+}
+
+pub fn pull_key_from_redis(key: &str) -> &str {
+  // Noting here yet....
+}
