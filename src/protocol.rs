2a8e17feb74536fabaf8f0a808c55b1fe79739bd
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-27 09:47:33 +1100

    function to create a usable struct of the string components

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json;
+use str::split;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
@@ -8,7 +9,24 @@ pub struct Message {
     is_test: bool,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MessageString {
+    client_code: String,
+    alarm_event_code: String,
+    handshake_code: String,
+}
+
 pub fn message_from_data(data: &[u8]) -> Message {
   let v: Message = serde_json::from_slice(data).unwrap();
   return v;
+}
+
+pub fn parse_message_string(msg: Message) -> MessageString {
+    let mut msg_split = Message.message_string.split("-").collect();
+    let msgstr = MessageString {
+        client_code: msg_split[0],
+        alarm_event_code: msg_split[1],
+        handshake_code: msg_split[2]
+    }
+    return msgstr
 }
