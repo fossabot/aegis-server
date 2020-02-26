535c62203e91d46077ecef792e3066a4f29c0452
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-27 08:25:12 +1100

    parse incoming json to Message struct

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,1 +1,14 @@
-// Nothing here yet
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    message_string: String,
+    timestamp: String,
+    is_test: bool,
+}
+
+pub fn message_from_data(data: &str) -> Message {
+  let v: Message = serde_json::from_str(data);
+  return v;
+}
