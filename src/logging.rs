6f27ba68fa2683ac3a4f2630a2d46a1e6c8bf777
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-27 07:26:43 +1100

    init logging without returning an object

--- src/logging.rs
+++ src/logging.rs
@@ -2,17 +2,15 @@ extern crate log;
 extern crate simplelog;
 
 use simplelog::*;
-use std::boxed::Box;
 use std::fs::File;
 
-pub fn logger(logfile: &str) -> Box<CombinedLogger> {
-    let new_logger = CombinedLogger::new(vec![
+pub fn logger(logfile: &str) {
+    CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed).unwrap(),
         WriteLogger::new(
             LevelFilter::Info,
             Config::default(),
             File::create(logfile).unwrap(),
         ),
-    ]);
-    return new_logger;
+    ]).unwrap();
 }
