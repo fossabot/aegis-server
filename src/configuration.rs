73dec3a9e7e025fde97395d7d1f3ebe5b639cca5
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-27 12:41:47 +1100

    configuration module

---
+++ src/configuration.rs
@@ -0,0 +1,17 @@
+use serde_derive::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+pub struct Config {
+    ip: String,
+    port: Option<u16>,
+    debug: bool,
+    handshake: Option<u16>,
+    redis: String,
+}
+
+pub fn parse_config() -> Config {
+    let conffile = fs::read_to_string("/etc/aegis.toml");
+    let config: Config = toml::from_str(conffile).unwrap();
+    return config;
+}
