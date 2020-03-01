4747b741f2385dbc8c2c8affb1bc6828da9093ab
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-03-02 07:20:18 +1100

    Redis config has its own struct

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,11 +2,17 @@ use serde_derive::Deserialize;
 use std::fs;
 
 #[derive(Deserialize)]
+pub struct Redis {
+    pub host: String,
+    pub channel: String
+}
+
+#[derive(Deserialize)]
 pub struct Config {
     pub ip: String,
     pub debug: bool,
     pub handshake: Option<u16>,
-    pub redis: String,
+    pub redis: Redis,
 }
 
 pub fn parse_config() -> Config {
