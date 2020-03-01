93757a359696681574420972b909440d50bc8a38
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-03-02 10:20:18 +1100

    utility functions to create the redis client and build the URL string

--- src/output/redis_pubsub.rs
+++ src/output/redis_pubsub.rs
@@ -1,1 +1,16 @@
 //! take redis client connection, subscribe to the `aegis_alarm` channel.
+
+extern crate redis;
+
+//! build a connection URL
+//! > NOTE: Aegis requires an authenticated redis connection.
+pub fn build_redis_url(user: &str, passw: &str, host: &str) -> &str {
+  let rurl = format!("redis://{}:{}@{}", user, passw, host);
+  return rurl;
+}
+
+//! Creates a redis client for use in later pub/sub operations
+pub fn create_redis_client(redis_urL: &str) -> () {
+  let client = redis::Client::open(redis_url).unwrap();
+  return &client;
+}
