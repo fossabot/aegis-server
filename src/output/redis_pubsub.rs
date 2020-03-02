92690f48e95a74c0187bd7f62fa74bd76ae15050
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-03-02 12:04:17 +1100

    publish function

--- src/output/redis_pubsub.rs
+++ src/output/redis_pubsub.rs
@@ -1,6 +1,7 @@
 //! take redis client connection, subscribe to the `aegis_alarm` channel.
 
 extern crate redis;
+use redis::{Client, Commands, ControlFlow, PubSubCommands};
 
 //! build a connection URL
 //! > NOTE: Aegis requires an authenticated redis connection.
@@ -13,4 +14,10 @@ pub fn build_redis_url(user: &str, passw: &str, host: &str) -> &str {
 pub fn create_redis_client(redis_urL: &str) -> () {
   let client = redis::Client::open(redis_url).unwrap();
   return &client;
+}
+
+//! Publish a message to the redis channel
+pub fn publish(client: (), message_string: &str, channel: &str) {
+  let conn = client.get_connection().unwrap();
+  let _: ()  = conn.publish(channel, message_string).unwrap();
 }
